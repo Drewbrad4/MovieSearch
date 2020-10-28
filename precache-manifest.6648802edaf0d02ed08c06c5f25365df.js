df527dfb62d2f785c89f2a91c9e6a7257bb2518b
drewbrad4 <drewbrad4@gmail.com>
2020-10-27 18:04:18 -0700

    Updates

---
+++ precache-manifest.6648802edaf0d02ed08c06c5f25365df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc828ade31e9a0a128d76e8375ea2d50",
+    "url": "/MovieSearch/index.html"
+  },
+  {
+    "revision": "039ba386591e6a01763d",
+    "url": "/MovieSearch/static/css/main.ba77f2aa.chunk.css"
+  },
+  {
+    "revision": "d7361f799cc7c6fa6216",
+    "url": "/MovieSearch/static/js/2.b94ce951.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MovieSearch/static/js/2.b94ce951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039ba386591e6a01763d",
+    "url": "/MovieSearch/static/js/main.60c212bc.chunk.js"
+  },
+  {
+    "revision": "d61f6bfa7ec6bd01aa62",
+    "url": "/MovieSearch/static/js/runtime-main.f1f9c548.js"
+  }
+]);
