848dce8cffba98ad5f2ace6fcc66f6dafd761c5a
drewbrad4 <drewbrad4@gmail.com>
2020-10-27 17:19:40 -0700

    adjusted styling

--- src/Components/MovieSearch.js
+++ src/Components/MovieSearch.js
@@ -1,30 +1,59 @@
 import React, { Component } from "react";
+import MovieCard from './MovieCard.js'
 
 export default class SearchMovies extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            query: "", 
+            movies: [],
+            placeholder: "i.e. JAWS"
+        }
+    }
 
     searchLogic = async (e) => {
         e.preventDefault();
-        console.log("Submitting");
 
-        const query = "Jurassic Park"
-
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=5b517189a0a5351534c3313c224fc4b5&language=en-US&query=${query}&page=1&include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=5b517189a0a5351534c3313c224fc4b5&language=en-US&query=${this.state.query}&page=1&include_adult=false`;
 
         fetch(url);
 
-        const res = await fetch(url);
-        const data  = await res.json();
-        console.log(data);
+        try{
+            const res = await fetch(url);
+            const data  = await res.json();
+            let results = data.results
+            if(results == null){
+                let errorMessage = "Please enter a movie."
+                this.setState({placeholder: errorMessage})
+            }
+            this.setState({movies: results})
+        }catch(err){
+            console.error(err);
+        }
+    }
+
+    setQuery = (e) => {
+        let newQuery = e.target.value
+        this.setState({query: newQuery})
     }
 
     render(){
         return (
-            <form className="form" onSubmit={this.searchLogic}>
-                <label className="label" htmlFor="query">Movie Name</label>
-                <input className="input" type="text" name="query"
-                    placeholder="i.e. JAWS"/>
-                <button className="button" type="submit">Search</button>
-            </form>
+            <>
+                <form className="form" onSubmit={this.searchLogic}>
+                    <label className="label" htmlFor="query">Movie:</label>
+                    <input className="input" type="text" name="query"
+                        placeholder={this.state.placeholder}
+                        value={this.state.query}
+                        onChange={this.setQuery}/>
+                    <button className="button" type="submit">Search</button>
+                </form>
+                <div className="card-list">
+                {this.state.movies.filter(movie => movie.poster_path).map(movie => (
+                    <MovieCard movie={movie} />
+                ))}
+                </div> 
+            </>
         )
     }
 }
